ce1660325b2b207e697a1fa82caf3ab207b5ba0d
Sam <soc27@humboldt.edu>
2017-07-10 15:28:10 -0700

    Fix strip_whitespace bug, add some comments

--- string-things.cpp
+++ string-things.cpp
@@ -10,8 +10,13 @@ bool is_number(const std::string &str) {
     return true;
 }
 
+// Removes the whitespace from both ends of a string
 std::string strip_whitespace(std::string str) {
     auto str_start = str.find_first_not_of(" \t");
+    // Check to see if a string is all whitespace
+    if (str_start == std::string::npos) {
+        return "";
+    }
     auto str_end = str.find_last_not_of(" \t");
     return str.substr(str_start, str_end - str_start + 1);
 }
